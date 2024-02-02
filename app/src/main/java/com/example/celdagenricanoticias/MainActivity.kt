e65d4c95ae0241ccb655e7a7b84f7d6bc33e5c2c
Moises Abraham Vazquez Perez <mavazquezp@elektra.com.mx>
2024-02-02 14:55:06 -0600

    [UPDATE] Update view with new features on paddings, and adding a new field to show category on top

--- app/src/main/java/com/example/celdagenricanoticias/MainActivity.kt
+++ app/src/main/java/com/example/celdagenricanoticias/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.celdagenricanoticias
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,9 +22,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -67,15 +72,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ViewContainer() {
 
-    val arrNews: MutableList<ModelExample> = arrayListOf( ModelExample("Titulo uno noticia", subtitle = "Subtitulo uno noticas", date = "fecha de hoy mero", image = "ta_bien"),
-        ModelExample("Titulo Dos noticia", subtitle = "Subtitulo Dos noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo uno noticia", subtitle = "Subtitulo uno noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo Tres noticia", subtitle = "Subtitulo Tres noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo Cuatro noticia", subtitle = "Subtitulo Cuatro noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo Cinco  noticia", subtitle = "Subtitulo Cinco noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo Seis noticia", subtitle = "Subtitulo Seis noticas", date = "fecha de hoy mero", image = "ta_bien"),
-    ModelExample("Titulo Siete noticia", subtitle = "Subtitulo Siete noticas", date = "fecha de hoy mero", image = "ta_bien"),
-            ModelExample("Titulo Ocho noticia", subtitle = "Subtitulo Ocho noticas", date = "fecha de hoy mero", image = "ta_bien"))
+    val arrNews: MutableList<ModelExample> = arrayListOf( ModelExample("Titulo uno noticia", subtitle = "Subtitulo uno noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Politica"),
+        ModelExample("Titulo Dos noticia", subtitle = "Subtitulo Dos noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Ciencia y salud"),
+    ModelExample("Titulo uno noticia", subtitle = "Subtitulo uno noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Entretenimiento"),
+    ModelExample("Titulo Tres noticia", subtitle = "Subtitulo Tres noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Farandula"),
+    ModelExample("Titulo Cuatro noticia", subtitle = "Subtitulo Cuatro noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Redes Sociales"),
+    ModelExample("Titulo Cinco  noticia", subtitle = "Subtitulo Cinco noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Juegos"),
+    ModelExample("Titulo Seis noticia", subtitle = "Subtitulo Seis noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Mundo"),
+    ModelExample("Titulo Siete noticia", subtitle = "Subtitulo Siete noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Economia"),
+            ModelExample("Titulo Ocho noticia", subtitle = "Subtitulo Ocho noticas", date = "fecha de hoy mero", image = "ta_bien", category = "Deportes"))
 
     Surface(
         color = MaterialTheme.colorScheme.background
@@ -88,13 +93,17 @@ fun ViewContainer() {
                 .padding(horizontal = 16.dp)
         ) {
             
-            TopAppBar(title = { 
-                Text(text = "Noticias")
-            })
+            TopAppBar(title = { Text(text = "Noticias") },
+                actions = {
+                    IconButton(onClick = { /* que hacer cuando le den tap al boton (NAVEGACION PADRINO)*/ }) {
+                        Icon(imageVector = Icons.Default.Settings, contentDescription = "filtro")
+                    }
+                }
+            )
 
             ViewCategoryNew()
             
-            Spacer(modifier = Modifier.height(5.dp))
+            Spacer(modifier = Modifier.height(2.dp))
 
             LazyColumn(modifier = Modifier.weight(1f)) {
                 items(arrNews) {
@@ -122,7 +131,7 @@ fun GenericCell(item: ModelExample) {
 
             Image(painter = painterResource(id = R.mipmap.ta_bien), contentDescription = "",
                 modifier = Modifier
-                    .padding(start = 16.dp)
+                    //.padding(start = 16.dp)
                     .width(56.dp)
                     .height(56.dp))
 
@@ -130,8 +139,12 @@ fun GenericCell(item: ModelExample) {
                 modifier = Modifier.padding(start = 4.dp),
                 verticalArrangement = Arrangement.Top) {
 
+                Text(text = item.category,
+                    modifier = Modifier.padding(top = 2.dp, start = 16.dp),
+                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold,
+                        fontSize = 10.sp, color = Color.Gray))
                 Text(text = item.titulo,
-                    modifier = Modifier.padding(top = 16.dp, start = 16.dp),
+                    modifier = Modifier.padding(top = 2.dp, start = 16.dp),
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold,
                         fontSize = 14.sp))
                 Text(text = item.subtitle,
@@ -139,7 +152,7 @@ fun GenericCell(item: ModelExample) {
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Medium,
                         fontSize = 12.sp))
                 Text(text = item.date,
-                    modifier = Modifier.padding(top = 0.dp, start = 16.dp),
+                    modifier = Modifier.padding(top = 8.dp, start = 16.dp),
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Thin,
                         fontSize = 10.sp))
             }
@@ -185,7 +198,6 @@ fun GenericCell() {
 
             Image(painter = painterResource(id = R.mipmap.ta_bien), contentDescription = "",
                 modifier = Modifier
-                    .padding(start = 16.dp)
                     .width(56.dp)
                     .height(56.dp))
 
@@ -193,8 +205,12 @@ fun GenericCell() {
                 modifier = Modifier.padding(start = 4.dp),
                 verticalArrangement = Arrangement.Top) {
 
+                Text(text = "categoria",
+                    modifier = Modifier.padding(top = 2.dp, start = 16.dp),
+                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold,
+                        fontSize = 10.sp, color = Color.Gray))
                 Text(text = "Titulo Noticia",
-                    modifier = Modifier.padding(top = 0.dp, start = 16.dp),
+                    modifier = Modifier.padding(top = 2.dp, start = 16.dp),
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold,
                         fontSize = 14.sp))
                 Text(text = "Subtitulo Noticia",
@@ -202,7 +218,7 @@ fun GenericCell() {
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Medium,
                         fontSize = 12.sp))
                 Text(text = "Miercoles 23 de Enero del 2024",
-                    modifier = Modifier.padding(top = 0.dp, start = 16.dp),
+                    modifier = Modifier.padding(top = 8.dp, start = 16.dp),
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Thin,
                         fontSize = 10.sp))
             }
@@ -277,38 +293,39 @@ fun FooterTVInfo() {
 }
 
 
+
 @Preview
 @Composable
 
 fun ViewCategoryNew() {
     Column(modifier = Modifier
-        .height(60.dp)
+        .height(68.dp)
         .background(Color.White),
         verticalArrangement = Arrangement.spacedBy(8.dp))
     {
-
         LazyRow(modifier = Modifier
             .fillMaxWidth()
-            .height(68.dp)) {
+            .padding(4.dp)
+            .height(60.dp),
+            verticalAlignment = Alignment.CenterVertically) {
             items(5) {
                 index -> CategoryButton()
             }
         }
-
+        Spacer(modifier = Modifier.width(6.dp))
     }
 }
 
 @Preview
 @Composable
-
 fun CategoryButton() {
     Button(onClick = { },
         modifier = Modifier
-            .height(38.dp)
-            .border(1.dp, Color.Green, RoundedCornerShape(4.dp))
+            .height(36.dp)
+            .padding(start = 2.dp, end = 2.dp)
+            .border(1.dp, Color.White, RoundedCornerShape(0.5.dp))
             .background(Color.White,)
     ) {
-
         Text(text = "(Categorias)",
             color = Color.White)
     }
@@ -318,5 +335,6 @@ data class ModelExample (
     val titulo: String,
     val subtitle: String,
     val date: String,
-    val image: String
+    val image: String,
+    var category: String
 )
